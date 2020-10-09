8c5e5b2381e814baa0d81e49f2b7afcea47bb0d8
JOEL ORRYN HAAG <220149181@student.uj.ac.za>
2020-10-09 12:29:57 +0200

    1 - created file variables and some animal variables 2 - created the sub routine to serialize the derived classes

--- MainForm.Designer.vb
+++ MainForm.Designer.vb
@@ -22,49 +22,12 @@ Partial Class frm_Main
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.grid = New UJGrid.UJGrid()
-        Me.btn_Load = New System.Windows.Forms.Button()
-        Me.btn_Save = New System.Windows.Forms.Button()
         Me.SuspendLayout()
-        '
-        'grid
-        '
-        Me.grid.FixedCols = 1
-        Me.grid.FixedRows = 1
-        Me.grid.Location = New System.Drawing.Point(475, 31)
-        Me.grid.Name = "grid"
-        Me.grid.Scrollbars = System.Windows.Forms.ScrollBars.Both
-        Me.grid.Size = New System.Drawing.Size(450, 157)
-        Me.grid.TabIndex = 0
-        '
-        'btn_Load
-        '
-        Me.btn_Load.Location = New System.Drawing.Point(475, 194)
-        Me.btn_Load.Name = "btn_Load"
-        Me.btn_Load.Size = New System.Drawing.Size(97, 24)
-        Me.btn_Load.TabIndex = 1
-        Me.btn_Load.Text = "Load Data.."
-        Me.btn_Load.UseVisualStyleBackColor = True
-        '
-        'btn_Save
-        '
-        Me.btn_Save.Location = New System.Drawing.Point(578, 194)
-        Me.btn_Save.Name = "btn_Save"
-        Me.btn_Save.Size = New System.Drawing.Size(97, 24)
-        Me.btn_Save.TabIndex = 2
-        Me.btn_Save.Text = "Save Data.."
-        Me.btn_Save.UseVisualStyleBackColor = True
         '
         'frm_Main
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1068, 443)
-        Me.Controls.Add(Me.btn_Save)
-        Me.Controls.Add(Me.btn_Load)
-        Me.Controls.Add(Me.grid)
+        Me.ClientSize = New System.Drawing.Size(284, 261)
         Me.Name = "frm_Main"
-        Me.Text = "Main Form"
         Me.ResumeLayout(False)
 
     End Sub
--- MainForm.vb
+++ MainForm.vb
@@ -2,6 +2,7 @@
 Option Strict On
 Option Infer Off
 Imports System.IO
+Imports System.Runtime.Serialization.Formatters.Binary
 ' *****************************************************************
 ' Team Number: 17
 ' Team Member 1 Details: Musto, M.C.M (219104286)
@@ -15,6 +16,16 @@ Imports System.IO
 
 Public Class frm_Main
 
+    'File Variables
+    Private FS As FileStream
+    Private BF As BinaryFormatter
+    Private Const FNAME As String = "220149181.ipb"
+
+    'Variables for animal
+    Private Animals() As Animal
+    Private NumberOfAnimals As Integer = 0
+
+
     Private localFile As FileStream
     Private Sub frm_Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -23,5 +34,26 @@ Public Class frm_Main
     Private Sub btn_Load_Click(sender As Object, e As EventArgs) Handles btn_Load.Click
         Dim x As Integer
         x += 1
+    End Sub
+
+    Private Sub frm_Main_Load_1(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    'sub routine to serialize derived classes
+    Private Sub SerializeFiles()
+        FS = New FileStream(FNAME, FileMode.Open, FileAccess.Write)
+        BF = New BinaryFormatter()
+        For x As Integer = 1 To NumberOfAnimals
+            If Animals(x).GetType.ToString() = "GroupProject_Enviro.Lion" Then
+                Dim tempLion As Lion
+                tempLion = DirectCast(Animals(x), Lion)
+                BF.Serialize(FS, tempLion)
+            ElseIf Animals(x).GetType.ToString() = "GroupProject_Enviro.Addax" Then
+                Dim tempAddax As Addax
+                tempAddax = DirectCast(Animals(x), Addax)
+                BF.Serialize(FS, tempAddax)
+            End If
+        Next
     End Sub
 End Class
